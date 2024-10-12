3ae438d2c05df7b8282f4b340cc6dcb03cb1e966
Christina Alexander <christinaalexander@Christina-MacBook-Air.local>
2024-10-12 13:39:21 +0300

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+} 
+  
+// accessing password from command line parameters
+const password = process.argv[2]
+
+const url = `mongodb+srv://chralexander:${password}@cluster0.hfvfr.mongodb.net/Phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+  
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length===3) {
+    // displaying all the info from the Phonebook
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length>3)  {
+    //contains name and number
+    const personName = process.argv[3]
+    const personNumber = process.argv[4]
+
+    const person = new Person({
+        name: personName,
+        number: personNumber,
+    })
+
+    person.save().then(result => {
+        console.log(`added ${personName} number ${personNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+}
